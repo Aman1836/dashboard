e92d1ebe9a743d1e4780fcc0ac1cf3c3220b542c
Aman1836 <amandikoleninja@gmail.com>
2024-02-17 00:22:39 +0530

    2nd code

--- Dashboard/src/comp/MidBox.jsx
+++ Dashboard/src/comp/MidBox.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function MidBox() {
   return (
     <div className=' max-w-screen-sm  h-screen bg-red-600 '>
-        <div className='w-full h-9 bg-amber-400'>aman</div>
+        <div className='w-full h-9 bg-amber-400'>Aman Sajjan Dikole</div>
     </div>
   )
 }   
